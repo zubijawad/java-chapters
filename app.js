6fa401742520b87a7d595c0aa4b109b91038c6c0
zubijawad <zubijawad495@gmail.com>
2025-06-18 12:15:40 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+// chapter 1
+// answaer number 1
+ alert("javascript alert")
+ // answer number 2
+ alert("Error!please enter a volid password")
+ // answer number 3
+ alert("welcome to js land...\n happy coding")
+ // answer number 4
+ alert("welcome to js land...")
+ alert("Happy coding!\n prevent this page from creating adtional dialogs")
+ // answer number 5
+ console.log("Hello... i can run js through my web browser's console")
+//  answer number 6
+ alert("html")
+alert("css")
+// answer number 7
+<script>
+(a) <script> <head></head> 
+(b) <script> <body> <head>
+(c) <script> <body></body>
+(d) <script> <body>    closing</body>
+{/* chapter 2  */}
+ {/* answer1  */}
+var: var username;
+{/* answer 2 */}
+myName = "john doe"
+var myName = "jane Doe";
+string myName = "zulaikha"
+{/* answer 3 */}
+(a) var message;
+(b) message = "Hello World";
+(b) alert(message);
+{/* answer number 4 */}
+alert("Johne Doe ");
+alert("15 Years Old");
+alert("certified Mobile Application Development");
+{/* answer5 */}
+alert("PIZZA");
+alert("PIZZ");
+alert("PIZ");
+alert("PI");
+alert("P");
+{/* Answer6 */}
+var email
+alert("zubijawad@495gmail.com")
+{/* answer 7 */}
+var book = "A smart way to learn javascript";
+alert("A smart way to learn javascript" + book);
+{/* answer8 */}
+document.write("Yah! T can write HTML content through javascript");
+{/* answer9 */}
+alert("Mobile -END-")
+
+
+ 
